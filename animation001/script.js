e00ea336e098fc0f6f5e0882fd4a4db2d922b85b
Linkhant19 <lkk19@bu.edu>
2024-09-15 19:23:57 -0400

    changed cursor image

--- animation001/script.js
+++ animation001/script.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const star = document.getElementById("star");
     const found = document.getElementById("found");
+    const timer = document.getElementById("timer");
+
+    let startTime = performance.now();
+    let timerInterval;
+
+    function updateTimer() {
+        let currentTime = performance.now();
+        let elapsedTime = currentTime - startTime;
+        let seconds = Math.floor(elapsedTime / 1000);
+        let milliseconds = Math.floor(elapsedTime % 1000);
+
+        timer.textContent = `${seconds}s ${milliseconds}ms`;
+    }
+
+    timerInterval = setInterval(updateTimer, 10)
 
     star.addEventListener("mouseover", function() {
         star.classList.add("visible");
-        found.classList.add("visible")
+        found.classList.add("visible");
+        clearInterval(timerInterval);
     });
 
 });
